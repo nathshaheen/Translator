336c7f89559b31840237ebad426406576445373d
NathShaheen <nathjsmail@gmail.com>
2022-04-22 13:41:52 +1000

    Added SegmentControlView

---
+++ Translator/Views/SegmentControlView.swift
@@ -0,0 +1,20 @@
+//
+//  SegmentControlView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 22/4/2022.
+//
+
+import SwiftUI
+
+struct SegmentControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SegmentControlView_Previews: PreviewProvider {
+    static var previews: some View {
+        SegmentControlView()
+    }
+}
