e03bb2a3cffaf0ccc92f68b6ee8138090a462ac5
NathShaheen <nathjsmail@gmail.com>
2022-04-22 12:49:45 +1000

    Initial Commit

---
+++ Translator/TranslatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslatorApp.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 22/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct TranslatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
