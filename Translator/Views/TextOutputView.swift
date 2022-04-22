fbd2851b65b49777d7a0ee4aadafb503f073afc2
NathShaheen <nathjsmail@gmail.com>
2022-04-22 13:41:24 +1000

    Added TextOutputView

---
+++ Translator/Views/TextOutputView.swift
@@ -0,0 +1,20 @@
+//
+//  TextOutputView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 22/4/2022.
+//
+
+import SwiftUI
+
+struct TextOutputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextOutputView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextOutputView()
+    }
+}
