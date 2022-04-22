c8e9bbd2c8251ea2168ea13a3da6e487db0809d3
NathShaheen <nathjsmail@gmail.com>
2022-04-22 13:40:50 +1000

    Added TextInputView

---
+++ Translator/Views/TextInputView.swift
@@ -0,0 +1,20 @@
+//
+//  TextInputView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 22/4/2022.
+//
+
+import SwiftUI
+
+struct TextInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TextInputView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextInputView()
+    }
+}
