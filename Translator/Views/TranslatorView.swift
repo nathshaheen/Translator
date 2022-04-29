b9c4d4872f6ea3c02a6c36041dd276224e63528d
NathShaheen <nathjsmail@gmail.com>
2022-04-29 15:17:18 +1000

    Added TranslatorView

---
+++ Translator/Views/TranslatorView.swift
@@ -0,0 +1,20 @@
+//
+//  TranslatorView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 29/4/2022.
+//
+
+import SwiftUI
+
+struct TranslatorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TranslatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        TranslatorView()
+    }
+}
