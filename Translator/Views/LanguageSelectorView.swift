bac46027e09954c01f2240e2634eb42da3fe2f8b
NathShaheen <nathjsmail@gmail.com>
2022-04-29 15:15:49 +1000

    Added LanguageSelectorView and its basic structure

---
+++ Translator/Views/LanguageSelectorView.swift
@@ -0,0 +1,28 @@
+//
+//  LanguageSelectorView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 25/4/2022.
+//
+
+import SwiftUI
+
+struct LanguageSelectorView: View {
+    var body: some View {
+        HStack {
+            Text("Language 1")
+            
+            Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+            }
+            
+            Text("Language 2")
+        }
+    }
+}
+
+struct LanguageSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        LanguageSelectorView()
+    }
+}
