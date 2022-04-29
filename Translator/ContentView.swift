8102ce1221ce0c9a012c94fe074de93cf57e49be
NathShaheen <nathjsmail@gmail.com>
2022-04-29 15:40:59 +1000

    Added tab navigation functionality for translator and settings views

--- Translator/ContentView.swift
+++ Translator/ContentView.swift
@@ -9,8 +9,17 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            TranslatorView()
+                .tabItem {
+                    Label("Translator", systemImage: "globe")
+                }
+            
+            SettingsView()
+                .tabItem {
+                    Label("Settings", systemImage: "gear")
+                }
+        }
     }
 }
 
--- Translator/TranslatorApp.swift
+++ Translator/TranslatorApp.swift
@@ -18,6 +18,6 @@ struct TranslatorApp: App {
 
 struct Previews_TranslatorApp_Previews: PreviewProvider {
     static var previews: some View {
-        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+        ContentView()
     }
 }
