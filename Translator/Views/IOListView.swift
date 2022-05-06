ae0cc035bbfeb8e903baacfb23603396fc360782
NathShaheen <nathjsmail@gmail.com>
2022-05-06 15:20:39 +1000

    Added IOListView

---
+++ Translator/Views/IOListView.swift
@@ -0,0 +1,20 @@
+//
+//  IOListView.swift
+//  Translator
+//
+//  Created by Nathan Shaheen on 6/5/2022.
+//
+
+import SwiftUI
+
+struct IOListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct IOListView_Previews: PreviewProvider {
+    static var previews: some View {
+        IOListView()
+    }
+}
