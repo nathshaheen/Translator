8f1a0f36b550c9a7385b026cb6a6bcc1dccb22c0
NathShaheen <nathjsmail@gmail.com>
2022-05-24 19:29:32 +1000

    Added functionality

--- Translator/ImagePicker.swift
+++ Translator/ImagePicker.swift
@@ -5,16 +5,49 @@
 //  Created by Nathan Shaheen on 24/5/2022.
 //
 
+// The following is taken from https://www.hackingwithswift.com/books/ios-swiftui/importing-an-image-into-swiftui-using-phpickerviewcontroller
+// Author: Paul Hudson
+//  Email: paul@hackingwithswift.com
+
+import PhotosUI
 import SwiftUI
 
-struct ImagePicker: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+struct ImagePicker: UIViewControllerRepresentable {
+    @Binding var image: UIImage?
 
-struct ImagePicker_Previews: PreviewProvider {
-    static var previews: some View {
-        ImagePicker()
+    func makeUIViewController(context: Context) -> PHPickerViewController {
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: PHPickerViewController, context: Context) {
+
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    class Coordinator: NSObject, PHPickerViewControllerDelegate {
+        let parent: ImagePicker
+
+        init(_ parent: ImagePicker) {
+            self.parent = parent
+        }
+
+        func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+            picker.dismiss(animated: true)
+
+            guard let provider = results.first?.itemProvider else { return }
+
+            if provider.canLoadObject(ofClass: UIImage.self) {
+                provider.loadObject(ofClass: UIImage.self) { image, _ in
+                    self.parent.image = image as? UIImage
+                }
+            }
+        }
     }
 }
