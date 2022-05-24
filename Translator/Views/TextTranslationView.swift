0b89cf2a762d6ff9afe3f6581a4ad9fcdf97b57c
NathShaheen <nathjsmail@gmail.com>
2022-05-24 19:30:17 +1000

    Added styling and functionality

--- Translator/Views/TextTranslationView.swift
+++ Translator/Views/TextTranslationView.swift
@@ -8,13 +8,49 @@
 import SwiftUI
 
 struct TextTranslationView: View {
+    @State var inputText = "Enter text"
+    @State var translatedText = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.white)
+                
+                TextEditor(text: $inputText)
+                    .frame(maxHeight: ContentView.screenHeight * 0.25)
+                    .cornerRadius(10)
+                    .padding()
+                         
+                Divider()
+                
+                ScrollView {
+                    Text(" " + translatedText)
+                }
+                    .frame(maxWidth: .infinity, maxHeight: ContentView.screenHeight * 0.25, alignment: .topLeading)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .padding()
+                
+                Spacer()
+                
+                LanguageTranslationView(input: $inputText, output: $translatedText)
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.white)
+            }
+            .background(.ultraThinMaterial)
+            .overlay(Rectangle().frame(width: nil, height: 1, alignment: .top).foregroundColor(.secondary.opacity(0.1)), alignment: .top)
+            .overlay(Rectangle().frame(width: nil, height: 1, alignment: .bottom).foregroundColor(.secondary.opacity(0.1)), alignment: .bottom)
+            .navigationTitle("Text Translation")
+        }
     }
 }
 
-struct TextTranslationView_Previews: PreviewProvider {
-    static var previews: some View {
-        TextTranslationView()
-    }
-}
+//struct TextTranslationView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        TextTranslationView()
+//    }
+//}
