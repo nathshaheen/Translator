608deccccaa651012140afe185f77316a4cd7926
NathShaheen <nathjsmail@gmail.com>
2022-05-24 19:27:03 +1000

    Added data

--- Translator/ModelData.swift
+++ Translator/ModelData.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+import MLKit
+
+var languages: [Language] = [
+    Language(id: 0, name: "Afrikaans", languageIdentifier: .afrikaans),
+    Language(id: 1, name: "Arabic", languageIdentifier: .arabic),
+    Language(id: 2, name: "Chinese", languageIdentifier: .chinese),
+    Language(id: 3, name: "English", languageIdentifier: .english),
+    Language(id: 4, name: "French", languageIdentifier: .french),
+    Language(id: 5, name: "German", languageIdentifier: .german),
+    Language(id: 6, name: "Italian", languageIdentifier: .italian),
+    Language(id: 7, name: "Japanese", languageIdentifier: .japanese),
+    Language(id: 8, name: "Korean", languageIdentifier: .korean),
+    Language(id: 9, name: "Portuguese", languageIdentifier: .portuguese),
+    Language(id: 10, name: "Russian", languageIdentifier: .russian),
+    Language(id: 11, name: "Spanish", languageIdentifier: .spanish),
+]
