10c4fb4278eeb5e8e3d2216cb0b9a34aa9aeec0a
NathShaheen <nathjsmail@gmail.com>
2022-05-24 19:31:06 +1000

    Added styling and functionality

--- Translator/Views/ImageTranslationView.swift
+++ Translator/Views/ImageTranslationView.swift
@@ -7,14 +7,80 @@
 
 import SwiftUI
 
+import MLKitTextRecognition
+import MLKitVision
+
 struct ImageTranslationView: View {
+    @State var inputText = ""
+    @State var translatedText = ""
+    
+    @State var image: Image?
+    @State var showImagePicker = false
+    @State var inputImage: UIImage?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.white)
+                
+                ZStack {
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(10)
+                }
+                    .frame(maxWidth: .infinity, maxHeight: ContentView.screenHeight * 0.25, alignment: .center)
+                    .padding(.horizontal)
+                
+                Label("Image", systemImage: "photo")
+                    .onTapGesture {
+                        showImagePicker = true
+                    }
+                    .sheet(isPresented: $showImagePicker) {
+                        ImagePicker(image: $inputImage)
+                    }
+                
+                Divider()
+                
+                ScrollView {
+                    Text(" " + translatedText)
+                }
+                    .frame(maxWidth: .infinity, maxHeight: ContentView.screenHeight * 0.25, alignment: .topLeading)
+                    .background(.white)
+                    .cornerRadius(10)
+                    .padding()
+                
+                Spacer()
+                
+                LanguageTranslationView(input: $inputText, output: $translatedText)
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.white)
+            }
+            .background(.ultraThinMaterial)
+            .navigationTitle("Image Translation")
+            .onChange(of: inputImage) { _ in loadImage() }
+        }
+    }
+    
+    func loadImage() {
+        guard let inputImage = inputImage else { return }
+        image = Image(uiImage: inputImage)
+        
+        let textRecognizer = TextRecognizer.textRecognizer(options: TextRecognizerOptions())
+        let visionImage = VisionImage(image: inputImage)
+        visionImage.orientation = inputImage.imageOrientation
+        textRecognizer.process(visionImage) { result, error in guard error == nil, let result = result else { return }
+                inputText = result.text
+        }
     }
 }
 
-struct ImageTranslationView_Previews: PreviewProvider {
-    static var previews: some View {
-        ImageTranslationView()
-    }
-}
+//struct ImageTranslationView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ImageTranslationView()
+//    }
+//}
